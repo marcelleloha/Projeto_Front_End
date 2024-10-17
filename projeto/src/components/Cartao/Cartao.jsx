cbe4ff47c57fccc4417bda335b7a021e656d0a64
BarbaraMalta <arthurschiller10@gmail.com>
2024-10-17 11:30:28 -0300

    teste

--- projeto/src/components/Cartao/Cartao.jsx
+++ projeto/src/components/Cartao/Cartao.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import { Container } from './Style'
 import { Link } from "react-router-dom";
+import dados from '../../data/projects.json';
+import { useParams } from 'react-router-dom';
 
 function Cartao(props) {
+  const { id } = useParams();
+
+  let dadosFiltrados;
+  // filter: vai percorrer os elementos que o resultado da função seja true
+  // const dadosFiltrados = id ? dados.filter((elemento) => elemento.id === parseInt(id)) : dados;
+  if (id) {
+    dadosFiltrados = dados.filter((elemento) => elemento.id === parseInt(id));
+  } else {
+    dadosFiltrados = dados;
+  }
   return (
+    
     <Container>
+      
       <Link to='/projeto' id='no-underline'>
         <div id='divPrincipal'>
             <h3 id='titulo'>{props.titulo}</h3>
@@ -14,6 +28,25 @@ function Cartao(props) {
         </div>
       
       </Link>
+      
+        
+        
+          
+        {/*
+          
+          {dadosFiltrados.map(
+            (el, index) => (
+              <Link to={`/projeto/${el.id}`} id='no-underline'>
+                <div key={index} id='divPrincipal'>
+                  <h2>{el.titulo}</h2>
+                  <img src={el.imagem} alt={el.titulo} id='teste'/>
+                  <p id='texto'>{el.texto}</p>
+                </div>
+              </Link> 
+              )
+          )}
+          */} 
+          
       </Container>
     )
 }
