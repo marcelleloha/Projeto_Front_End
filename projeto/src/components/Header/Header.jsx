b8574167fca2fa703e87222f1784468e61f4415d
BarbaraMalta <arthurschiller10@gmail.com>
2024-10-17 08:22:03 -0300

    login

--- projeto/src/components/Header/Header.jsx
+++ projeto/src/components/Header/Header.jsx
@@ -13,11 +13,13 @@ const Header = () => (
         <Link to="/">Home</Link>
         <Link to="/fotos">Projetos</Link>
         <Link to="/sobrenos">Sobre o Projeto</Link>
-        
-        { window.sessionStorage.getItem('accessToken')
-        ? <Link to="/logout">Logout</Link>
-        : <Link to="/login">Login</Link>
-        }
+        {/*
+          
+          { window.sessionStorage.getItem('accessToken')
+          ? <Link to="/logout">Logout</Link>
+          : <Link to="/login">Login</Link>
+          }
+          */}
         
       
       </div>
