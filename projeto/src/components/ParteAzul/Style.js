be502a55cfe5d9576138493630c7a00f590ce412
BarbaraMalta <arthurschiller10@gmail.com>
2024-10-30 17:58:02 -0300

    inicio media query

--- projeto/src/components/ParteAzul/Style.js
+++ projeto/src/components/ParteAzul/Style.js
@@ -1,42 +1,70 @@
 import styled from "styled-components"
 
 const TopPart = styled.header `
-  background-color: rgb(0, 37, 85);
-  padding-left: 100px;
-  padding-right: 100px;
 
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  #topnav {
-    margin-top: 20px;
-    display: flex;
-    justify-content: center;
-
-  }
-  #titulo {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
+  @media (min-width: 1000px) {
     
+    background-color: rgb(0, 37, 85);
+    padding-left: 100px;
+    padding-right: 100px;
+  
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    #titulo {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+      
+    }
+    #tituloreal {
+      font-weight: bold;
+      font-size: 54px;
+    }
+    #texto {
+      font-size: 18px;
+      width: 450px;
+      font-weight: lighter;
+    }
+    p {
+      color: white;
+    }
+    img {
+      position: relative;
+      top: 4px;
+      width: 500px;
+    }
   }
-  #tituloreal {
-    font-weight: bold;
-    font-size: 54px;
+  
+  @media (max-width:400px) {
+    padding-left: 20px;
+    padding-right: 20px;
+    background-color: rgb(0, 37, 85);
+    padding-top: 20px;
+    #titulo {
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+      width: fit-content;
+    }
+    #texto {
+      font-size: 18px;
+      font-weight: lighter;
+    }
+    #tituloreal {
+      font-weight: bold;
+      font-size: 32px;
+    }
+    p {
+      color: white;
+    }
+    img {
+
+      width: 300px;
+
+    }
   }
-  #texto {
-    font-size: 18px;
-    width: 450px;
-    font-weight: lighter;
-  }
-  p {
-    color: white;
-  }
-  img {
-    height: 27rem;
-    position: relative;
-    top: 4px;
-  }
+
 
 `
 
